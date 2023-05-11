3cf284203669282c09e8c9e372cdb096502a1b43
YuJinUk <yujinuk5140@gmail.com>
2023-05-11 17:25:19 +0900

    Vue

---
+++ Vue/day9/09_skeleton/front-server/src/views/ArticleView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Article Page</h1>
+    <hr>
+    <router-link :to="{ name: 'CreateView' }">[Create]</router-link>
+    <ArticleList />
+  </div>
+</template>
+
+<script>
+
+import ArticleList from '@/components/ArticleList.vue'
+
+export default {
+  name: 'ArticleView',
+  components: {
+    ArticleList
+  },
+  computed:{
+  },
+  created() {
+    this.getArticles()
+  },
+  methods: {
+    getArticles() {
+        this.$store.dispatch('getArticles')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
